a994ff559e85e202f7b8a0800a56550181ce708a
Tanmay Vij <tanmayvij047695@gmail.com>
2026-07-23 21:42:04 +0530

    perf: move functions to asia-east2 to reduce trans-pacific routing latency

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -2,4 +2,4 @@ import {setGlobalOptions} from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 
 initializeApp();
-setGlobalOptions({maxInstances: 10});
+setGlobalOptions({maxInstances: 10, region: "asia-east2"});
